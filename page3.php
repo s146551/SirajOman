7f41563553328d2a8c78d37bf6153b38aff83980
s146551 <s146551@student.squ.edu.om>
2025-12-20 14:51:59 +0400

    - More fixes...

--- page3.php
+++ page3.php
@@ -1,4 +1,37 @@
 <?php
+require_once 'db_config.php';
+
+$message = ''; // Initialize message variable
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Get form data and sanitize
+    $governorate = $conn->real_escape_string($_POST['governorate']);
+    $wilaya = $conn->real_escape_string($_POST['wilaya']);
+    $attraction = $conn->real_escape_string($_POST['attraction']);
+
+    // Prepare and bind
+    $stmt = $conn->prepare("INSERT INTO locations (governorate, wilaya, attraction) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $governorate, $wilaya, $attraction);
+
+    // Execute and check
+    if ($stmt->execute()) {
+        $message = "<div class='alert alert-success mt-3'>New location added successfully!</div>";
+    } else {
+        $message = "<div class='alert alert-danger mt-3'>Error: " . $stmt->error . "</div>";
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+
 $pageTitle = "Add a new Location";
 include 'header.php';
 ?>
@@ -8,6 +41,7 @@ include 'header.php';
     
         <h1 class="text-primary">Add a New Location</h1>
         <p class="text-secondary">Use this form to add a new tourist location to the list on the "Locations & Places" page.</p>
+        <?php echo $message; ?>
 
         <form action="page3.php" method="post" id="add_location_form" class="mt-4" onsubmit="return validateAddLocation()">
 
